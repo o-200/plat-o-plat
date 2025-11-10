b244e16580f5605d9baeddd14912b1a5b2830bca
o-200 <omgout.200@gmail.com>
2025-11-10 15:32:40 +0700

    ogo da eto zje fix!

--- update-monorepo.sh
+++ update-monorepo.sh
@@ -1,9 +1,40 @@
 #!/bin/sh
+set -e
 
-git fetch plat-up
-git fetch oplat-up
+# Remotes
+PLAT_REMOTE="plat-up"
+OPLAT_REMOTE="oplat-up"
 
-git subtree pull --prefix=plat plat-up main --squash
-git subtree pull --prefix=oplat oplat-up main --squash
+# Branch to sync
+BRANCH="main"
 
+# Subtree prefixes
+PLAT_PREFIX="plat"
+OPLAT_PREFIX="oplat"
+
+# Function to add or pull a subtree
+update_subtree() {
+  PREFIX=$1
+  REMOTE=$2
+  BRANCH=$3
+
+  # Check if the prefix folder exists
+  if [ -d "$PREFIX" ]; then
+    echo "Updating subtree $PREFIX..."
+    git subtree pull --prefix="$PREFIX" "$REMOTE" "$BRANCH" --squash
+  else
+    echo "Adding subtree $PREFIX..."
+    git subtree add --prefix="$PREFIX" "$REMOTE" "$BRANCH" --squash
+  fi
+}
+
+# Fetch remotes
+git fetch "$PLAT_REMOTE"
+git fetch "$OPLAT_REMOTE"
+
+# Update or add subtrees
+update_subtree "$PLAT_PREFIX" "$PLAT_REMOTE" "$BRANCH"
+update_subtree "$OPLAT_PREFIX" "$OPLAT_REMOTE" "$BRANCH"
+
+# Push changes to origin
 git push
